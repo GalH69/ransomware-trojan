4afd3da4ceb476551b7de93f11a690fdbf9d78b3
GalH69 <auto.install@cs.com>
2025-11-09 01:18:44 +0200

    'autosave'

--- protocol.py
+++ protocol.py
@@ -1,19 +1,21 @@
-END_MARKER = "__END__"
+END_MARKER = b"__END__"
 
-def send(sock, message: str):
-    if not isinstance(message, str):
-        raise ValueError("Only strings are allowed in send()")
-    
-    sock.sendall((message + END_MARKER).encode("utf-8"))
-    
-def receive(sock) -> str:
+def send(sock, data: bytes | str):
+    if isinstance(data, str):
+        data = data.encode("utf-8")  # הופך טקסט לבייטים
+    elif not isinstance(data, bytes):
+        raise ValueError("send() only accepts str or bytes")
+
+    sock.sendall(data + END_MARKER)
+
+def receive(sock) -> bytes:
     buffer = b""
     while True:
         chunk = sock.recv(1024)
         if not chunk:
             break
         buffer += chunk
-        if buffer.endswith(END_MARKER.encode("utf-8")):
+        if buffer.endswith(END_MARKER):
             break
 
-    return buffer.decode("utf-8").removesuffix(END_MARKER)
+    return buffer.removesuffix(END_MARKER)
