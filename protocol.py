111172c76e2cb01342cb138bbf926b856d2491b7
GalH69 <auto.install@cs.com>
2025-11-08 22:46:39 +0200

    'autosave'

--- protocol.py
+++ protocol.py
@@ -5,3 +5,15 @@ def send(sock, message: str):
         raise ValueError("Only strings are allowed in send()")
     
     sock.sendall((message + END_MARKER).encode("utf-8"))
+    
+def receive(sock) -> str:
+    buffer = b""
+    while True:
+        chunk = sock.recv(1024)
+        if not chunk:
+            break
+        buffer += chunk
+        if buffer.endswith(END_MARKER.encode("utf-8")):
+            break
+
+    return buffer.decode("utf-8").removesuffix(END_MARKER)
