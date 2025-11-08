49ec13def183cfba927020ab31e091cd9ff9af83
GalH69 <auto.install@cs.com>
2025-11-08 19:21:46 +0200

    'autosave'

--- protocol.py
+++ protocol.py
@@ -1,1 +1,7 @@
-end_marker = b"__END__"
+END_MARKER = b"__END__"
+
+def send_message(sock, data: bytes):
+    if not isinstance(data, bytes):
+        raise TypeError("data must be bytes")
+
+    sock.sendall(data + END_MARKER)
