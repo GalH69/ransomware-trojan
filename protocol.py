9da0ba8e69fa62f6710a597f9d25bdcceeea743c
GalH69 <auto.install@cs.com>
2025-11-08 22:46:07 +0200

    'autosave'

--- protocol.py
+++ protocol.py
@@ -1,9 +1,7 @@
 END_MARKER = b"__END__"
 
-def send_message(sock, data: bytes):
-    if not isinstance(data, bytes):
-        raise TypeError("data must be bytes")
-    sock.sendall(data + END_MARKER)
+def send(sock, message: str):
+    if not isinstance(message, str):
+        raise ValueError("Only strings are allowed in send()")
     
-def send_text(sock, text: str):
-        send_message(sock, text.encode("utf-8"))
+    sock.sendall((message + END_MARKER).encode("utf-8"))
