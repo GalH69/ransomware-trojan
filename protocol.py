3ae86df6f351311181abe44dd4719024fa7dd309
GalH69 <auto.install@cs.com>
2025-11-09 02:05:25 +0200

    'autosave'

--- protocol.py
+++ protocol.py
@@ -9,17 +9,13 @@ def send(sock, data):
     else:
         raise ValueError("Only str or bytes are allowed in send()")
 
-def receive(sock, expect_bytes=False):
+def receive(sock):
     buffer = b""
     while True:
         chunk = sock.recv(1024)
         if not chunk:
             break
         buffer += chunk
-        if not expect_bytes and buffer.endswith(END_MARKER.encode("utf-8")):
+        if buffer.endswith(END_MARKER.encode("utf-8")):
             break
-
-    if expect_bytes:
-        return buffer
-    else:
-        return buffer.decode("utf-8").removesuffix(END_MARKER)
+    return buffer.decode("utf-8").removesuffix(END_MARKER)
