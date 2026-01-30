f6c39294d7bd8544470ec9916ea48aec174e7dec
GalH69 <auto.install@cs.com>
2026-01-30 12:39:13 +0200

    'autosave'

--- protocol.py
+++ protocol.py
@@ -1,29 +1,3 @@
-# END_MARKER = "__END__"
-
-# def send(sock, data):
-#     if type(data) is str:
-#         message = data + END_MARKER
-#         sock.sendall(message.encode("utf-8"))
-#     elif type(data) is bytes:
-#         message = data + END_MARKER.encode("utf-8")
-#         sock.sendall(message)
-#     else:
-#         raise ValueError("Only str or bytes are allowed in send()")
-
-# def receive(sock):
-#     buffer = b""
-#     while True:
-#         chunk = sock.recv(1024)
-#         if not chunk:
-#             break
-#         buffer += chunk
-#         if buffer.endswith(END_MARKER.encode("utf-8")):
-#             break
-#     data = buffer.removesuffix(END_MARKER.encode("utf-8"))
-#     return data #retern the data as binary
-
-
-
 MAX_LEN = 65535  # כי 2 bytes length (0..65535)
 
 def _recv_exact(sock, n):
