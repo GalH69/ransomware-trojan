3403b4f0184e630f3e68ad30a8db5b50d0946f6d
GalH69 <auto.install@cs.com>
2026-01-30 14:15:53 +0200

    'autosave'

--- server.py
+++ server.py
@@ -30,6 +30,7 @@ class SecureSocketServer:
         if msg.decode() == "DISCOVER_SERVER":
             print("Discovery from", addr)
             brodcast_sock.sendto(b"SERVER_HERE", addr)
+        return addr
 
     def _build_ssl_context(self):
         ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
