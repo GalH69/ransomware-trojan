1fdf04fbcedb9fc24ea473ab3e2e0bce70d05f8b
GalH69 <auto.install@cs.com>
2026-02-03 22:35:04 +0200

    'autosave'

--- victom folder/client.py
+++ victom folder/client.py
@@ -6,7 +6,7 @@ from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
 import protocol
-
+# asd
 
 class SecureSocketClient:
     # מחלקה שאחראית רק על יצירת socket עם TLS
